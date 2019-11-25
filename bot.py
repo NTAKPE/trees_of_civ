adb4bc103a7614a29d3e58305bb6f06e62e219ba
NTAKPE <jm.ntakpe@gmail.com>
2019-11-25 22:20:24 +0000

    Fix Twitter API bug

--- bot.py
+++ bot.py
@@ -6,25 +6,21 @@ from config import create_api
 import time
 import json
 
-#Authentificate the session
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-
 #Load trees container file
 with open ('csvjson.json', 'r') as file :
     data = file.read()
 
 #Generate text for tweet
 def get_spec(spec):
-    return "Arbre 🌳 de #CIV225 #{}/465 \nL'arbre du jour est le {}. \nSon Appelation latine est {}. \
+    return "Arbre 🌳 #{}/465 \nL'arbre du jour est le {}. \nSon appelation latine est {}. \
         \nLikez, retweetez, ajoutez photos & co en commentaires!\
-        \n#deforestation #eauxetforets #forets #JePlanteMonArbreCI #reboisement #BringBackOurTrees".format(spec['ID'],
+        \n#deforestation #eauxetforets #forets #JePlanteMonArbreCI #reboisement #BringBackOurTrees #CIV225".format(spec['ID'],
         spec['localName'], spec['latinName'])
 
 def main():
     api = create_api()
     specList = json.loads(data)
-    INTERVAL = 5
+    INTERVAL = 60*60*24
     for obj in specList :
         print('About to get tree')
         tree = get_spec(obj)

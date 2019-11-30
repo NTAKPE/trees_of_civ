e7b9e4a190cdf7b00397a3f4df4085dc99922dae
NTAKPE <jm.ntakpe@gmail.com>
2019-11-30 06:09:15 +0000

    Start from id 3

--- bot.py
+++ bot.py
@@ -13,15 +13,16 @@ with open ('csvjson.json', 'r') as file :
 #Generate text for tweet
 def get_spec(spec):
     return "🌳 Arbre #{}/465\nL'arbre du jour est le {} (nom latin :{}). \
-        \nSource: Atlas Des Bois De Côte d'Ivoire Tome 1\
-        \n#BringBackOurTrees #BoisDeCIV #CIV225".format(spec['ID'],
+        \nRappel: En 1950 la Côte d'Ivoire comptait 465 espèces de bois, \
+        les nommer, les sauver.\
+        \n#BringBackOurTrees #BoisDeCIV #CIV225 #GreenTech".format(spec['ID'],
         spec['localName'], spec['latinName'])
 
 def main():
     api = create_api()
     specList = json.loads(data)
     INTERVAL = 60*60*24
-    for obj in specList :
+    for obj in specList[3:] :
         print('About to get tree')
         tree = get_spec(obj)
         api.update_status(tree)

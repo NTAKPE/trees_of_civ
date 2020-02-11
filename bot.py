a969e8b4716d56bf06e075a07a173b4ebb3aeed5
NTAKPE <jm.ntakpe@gmail.com>
2020-02-11 17:04:57 +0000

    Change bot function for promotion

--- bot.py
+++ bot.py
@@ -3,31 +3,23 @@
 
 import tweepy
 from config import create_api
+from quotes import quotes
 import time
-import json
-
-#Load trees container file
-with open ('csvjson.json', 'r') as file :
-    data = file.read()
-
-#Generate text for tweet
-def get_spec(spec):
-    return "🌳 Arbre #{}/465\nL'arbre du jour est le {} (nom latin :{}). \
-        \nRappel: En 1950 la Côte d'Ivoire comptait 465 espèces de bois, \
-        les nommer, les sauver.\
-        \n#BringBackOurTrees #BoisDeCIV #CIV225 #GreenTech".format(spec['ID'],
-        spec['localName'], spec['latinName'])
 
 def main():
+    #Create twitter API
     api = create_api()
-    specList = json.loads(data)
-    INTERVAL = 60*60*24
-    for obj in specList[3:] :
-        print('About to get tree')
-        tree = get_spec(obj)
-        api.update_status(tree)
-        print("Waiting...")
-        time.sleep(INTERVAL)
+
+    #Set the loop parameters
+    INTERVAL_TWO = 60*60*12
+
+    while True:
+        INTERVAL_ONE = 60*60*6
+        quoteList = quotes()
+        for quote in quoteList :
+            api.update_status(quote)
+            time.sleep(INTERVAL_ONE)
+        time.sleep(INTERVAL_TWO)
 
 if __name__ == "__main__":
     main()

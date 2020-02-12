0dea0b0e30ff2f591fce7c041b0886934a6ebe1f
NTAKPE <jm.ntakpe@gmail.com>
2020-02-12 12:41:41 +0000

    Change img link

--- quotes.py
+++ quotes.py
@@ -2,6 +2,6 @@ def quotes():
     quoteList = [
     "Le BlackHistoryMonth débarque aussi à Babi 😍 CIV🇨🇮!\nParticipe au Diner Gala organisé pour le iBlackHistoryMonth, à l'INSAAC ce 29/02/2020.\n+ d’info ? https://cutt.ly/crHP0Zh | whatsapp: +225 40079826\n-10% sur ton ticket avec le code promo : #ivorianTouch",
     "A l’initiative de Jeunes d’Actifs de la place CIV🇨🇮, Babi célèbre pour la 1ère fois, le iBlackHistoryMonth avec un Dîner Gala à l'INSAAC ce 29/02/2020\n+ d’info ? https://cutt.ly/crHP0Zh | whatsapp: +225 40079826\n-10% sur ton ticket avec le code promo : #ivorianTouch",
-    "Babi de CIV🇨🇮 clôt son 1er Black History Month baptisé iBlackHistoryMonth avec un chic Diner Gala à l'INSAAC ce 29/02/2020.\n+ d’info ? https://cutt.ly/crHP0Zh | whatsapp: +225 40079826\n-10% sur ton ticket avec le code promo : #ivorianTouch"
+    "Babi de CIV🇨🇮 clôt son 1er Black History Month baptisé iBlackHistoryMonth avec un chic Diner Gala à l'INSAAC ce 29/02/2020.\n+ d’info ? https://cutt.ly/vrJKhc5 | whatsapp: +225 40079826\n-10% sur ton ticket avec le code promo : #ivorianTouch"
 ]
     return quoteList

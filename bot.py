9ba2c65500f3844f95b6cd6ba3d8f6cf2fd07b6e
NTAKPE <jm.ntakpe@gmail.com>
2020-07-21 22:22:33 +0000

    change function for rt and fav

--- bot.py
+++ bot.py
@@ -3,23 +3,49 @@
 
 import tweepy
 from config import create_api
-from quotes import quotes
+import json
 import time
+import logging
 
-def main():
-    #Create twitter API
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+class FavRetweetListener(tweepy.StreamListener):
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        logger.info(f"Processing tweet id {tweet.id}")
+        if tweet.in_reply_to_status_id is not None or \
+            tweet.user.id == self.me.id:
+            # This tweet is a reply or I'm its author so, ignore it
+            return
+        if not tweet.favorited:
+            # Mark it as Liked, since we have not done it yet
+            try:
+                tweet.favorite()
+            except :
+                logger.error("Error on fav", exc_info=True)
+        if not tweet.retweeted:
+            # Retweet, since we have not retweeted it yet
+            try:
+                tweet.retweet()
+            except:
+                logger.error("Error on fav and retweet", exc_info=True)
+
+    def on_error(self, status):
+        logger.error(status)
+
+def main(keywords):
     api = create_api()
+    tweets_listener = FavRetweetListener(api)
+    stream = tweepy.Stream(api.auth, tweets_listener)
+    stream.filter(track=keywords, languages=["fr", "en"])
 
-    #Set the loop parameters
-    INTERVAL_TWO = 60*60*3
-
-    while True:
-        INTERVAL_ONE = 60*60*4
-        quoteList = quotes()
-        for quote in quoteList :
-            api.update_status(quote)
-            time.sleep(INTERVAL_ONE)
-        time.sleep(INTERVAL_TWO)
+    #Wait 15 min before new loop through timeline
+    INTERVAL = 60*60/4
+    time.sleep(INTERVAL)
 
 if __name__ == "__main__":
-    main()
+    main(["paradoxe en Côte d’Ivoire", "la VAR"])
